fd6098b5d819413651fa167428975341f4267616
Bruno Vieira Costa <brunovcosta@outlook.com>
2022-05-19 16:55:16 -0300

    adding buying intention example

---
+++ python/buy.py
@@ -0,0 +1,31 @@
+"""
+This scripts is used in HackerForms (https://hackerforms.com) to collect buying intentions.
+This is a good example on how using Python scripts can be much simpler than forms + external automations
+"""
+
+from hackerforms import *
+from requests import post
+import os
+
+if 'plan' in url_params:
+  plan = url_params['plan']
+else:
+  plan = 'standard'
+
+display("Thank you for showing interest in our " + plan + " plan. We need some informations to get in touch.")
+name = read("Name")
+email = read_email('Email')
+company = read("Company name")
+
+token = os.environ.get("SLACK_BOT_TOKEN")
+
+res = requests.post(
+        'https://slack.com/api/chat.postMessage',
+    json={
+        'channel': 'sales',
+        'text': name + ' (' + email + ') wants to buy the ' + plan + ' plan'
+    },
+    headers={
+        'Authorization': 'Bearer ' + token,
+        'Content-type': 'application/json; charset=utf-8'
+    })
