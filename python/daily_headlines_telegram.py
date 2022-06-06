4239a0418412e40647303d3fe3c84096217feb23
ekacelnik <104526234+ekacelnik@users.noreply.github.com>
2022-06-06 17:40:29 -0300

    Add files via upload

---
+++ python/daily_headlines_telegram.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+import telepot
+
+
+url='https://techcrunch.com/'
+res = requests.get(url)
+
+soup = BeautifulSoup(res.text, 'html.parser')
+headlines = soup.find('body').find_all('h2')[:11]
+
+news = []
+
+for x in headlines:
+    news.append(x.text.strip())
+
+print(news)
+
+news_string = "\n\n".join(news)
+
+print(news_string)
+
+token = "Your_Token"
+chat_id = "Your_Chat_ID"
+bot = telepot.Bot(token)
+
+def send_message(text):
+   url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + chat_id + "&text=" + text 
+   results = requests.get(url_req)
+   print(results.json())
+
+send_message("Here are your daily Tech Crunch headlines:")
+send_message(news_string)
+send_message("For more, access techcrunch.com")
