e2fb5285969c0ba4804d9decff3911bb6583f9fe
ekacelnik <104526234+ekacelnik@users.noreply.github.com>
2022-06-26 13:26:42 -0300

    Update kpis_to_slack.py

--- python/kpis_to_slack.py
+++ python/kpis_to_slack.py
@@ -9,6 +9,8 @@ from datetime import datetime, timedelta
 from tabulate import tabulate
 
 # get KPIs from Metabase
+
+# This form uses an environment variable. To make it work properly, add a Metabase API Key to your workspace's environment variables in the sidebar.
 metabase_token = os.environ.get("METABASE_TOKEN")
 
 res = requests.post('https://metabase.YOUR_ENDPOINT/api/card/1/query/json', 
@@ -28,6 +30,8 @@ df_week = df[df_datetime > week]
 message = pd.DataFrame.to_markdown(df_week)
 
 # send to Slack
+
+# This form uses an environment variable. To make it work properly, add a Slack API Key to your workspace's environment variables in the sidebar.
 slack_token = os.environ.get("SLACK_TOKEN")
 
 res = requests.post(
